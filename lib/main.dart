8e8d93674219c6ee5b0a95d90c9046bd8938d8f4
Hanafiah Yahya <ibnuyahya@gmail.com>
2020-02-16 00:26:46 +0800

    Lab3 Final

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,35 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Lab',
+      title: 'Flutter Lab 3',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Flutter Lab'),
+          title: Text('Flutter Lab 3'),
         ),
         body: Container(
-
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Icon(Icons.phone),
+                  Container(child: Text('CALL'))
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Icon(Icons.navigation),
+                  Container(child: Text('ROUTE'))
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Icon(Icons.share),
+                  Container(child: Text('SHARE'))
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
